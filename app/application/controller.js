fa854b76b802012df22eb6e77ffb07af670cbc1a
Rajkiran147 <raki.kb91@gmail.com>
2020-05-15 10:37:59 +0530

    Update controller.js

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,7 +1,5 @@
 import Controller from '@ember/controller';
 
 export default class ApplicationController extends Controller {
-	get rootCategories() {
-		return this.model.rejectBy('parent.id');
-	}
+
 }
